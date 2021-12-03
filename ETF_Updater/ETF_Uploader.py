cd4c6762bc2dc09000892abe60f3398d9fa89d08
Amith <amithsebkanatt@gmail.com>
2021-12-03 23:32:03 +0530

    First commit of uploader
    Code to add the asset information in the DB

--- ETF_Updater/ETF_Uploader.py
+++ ETF_Updater/ETF_Uploader.py
@@ -1,30 +1,69 @@
 #To upload the ETF data into the DB for reporting purpose
-#This will be single program used to upload ETF from various index
+#To upload the ETF's NAV,AUM,Tracking error,Expense ratio
+#To upload the Asset's Tracking error,Expense ratio
 import pandas as PD
-from datetime import datetime
-from calendar import month_name
+import pymysql
 
-dateInfo=datetime.now()
-yearInfo=str(dateInfo.year)
-dateNumber=str(dateInfo.strftime("%d"))
-monthInfo=str(dateInfo.strftime("%b"))
-
-print("Date information",yearInfo)
-print("Year information",dateNumber)
-print("Month information",monthInfo)
-fileName='MW-ETF-'+dateNumber+'-'+monthInfo+'-'+yearInfo+'.csv'
-loc='/Users/amithkanatt/Downloads/'+fileName
-print("The file name is",fileName)
-print("The location information",loc)
+#Asset():
+#updateAsset():
+#createAsset():
+#createAssetdetails():
+#ETF():
+#createETF():
+#updateETF():
+#createETFdetails():
 
 
+#def updateAsset(connection_details):
 
+def addAsset(connection_details,data_loc):
+	try:
+		print("Adding asset information")
+		insert_sql="INSERT INTO`etf_asset`(`asset_info`)values(%s)"
+		cursor=connection_details.cursor()
+		AssetDetails = PD.read_excel(data_loc,sheet_name = 'Asset')
+		for lpcnt in AssetDetails.index:
+			values =AssetDetails['Asset'][lpcnt]
+			cursor.execute(insert_sql,values)
+		cursor.close()
+		
+	except Exception as e:
+		raise e
 
-#NIFTY():
-  #Read the data
-  #Upload the data
-#BSE():
-	#Read the data
-	#Upload the data
+#def createAssetdetails(connection_details):
 
+#def createETF(connection_details):
+
+#def addETF(connection_details):
+
+#def createETFdetails(connection_details):
+
+def connect_db():
+	try:
+		connection = pymysql.connect(host='localhost',user='root',password='',db='ETF')
+		#To check if the connection is successful
+		#Source:https://stackoverflow.com/questions/45800460/how-to-use-mysqldb-is-connected-to-check-an-active-mysql-connection-using-pyth
+		if connection.open:
+			print("Success")
+			return connection
+	except Exception as whaterror:
+		print(whaterror)
+
+def disconnect_db(connection_details):
+	try:
+		connection_details.commit()
+		connection_details.close()
+		if connection_details.open:
+			print("Something went wrong")
+		else:
+			print("Everything is success")
+	except Exception as whaterror:
+		print(whaterror)
+
+def main():
+	data_loc='/Volumes/Project/ETFAnalyser/ETF/ETF_Data/ETFdetail.xlsx'
+	connection_details=connect_db()
+	addAsset(connection_details,data_loc)
+	disconnect_db(connection_details)
+main()
 
