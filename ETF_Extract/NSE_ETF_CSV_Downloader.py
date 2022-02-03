10a5750a734a5ae4c17366774472e2e4842a81d4
Amith <amithsebkanatt@gmail.com>
2022-02-03 09:09:15 +0530

    Updated the code 1/ With browser termination using close() method instead of quit() 2/Commented the profile parameter,as undetected_chromedriver will generate profile automatically

--- ETF_Extract/NSE_ETF_CSV_Downloader.py
+++ ETF_Extract/NSE_ETF_CSV_Downloader.py
@@ -12,10 +12,11 @@ from selenium.webdriver.common.by import By
 if __name__ == '__main__':
 	options = webdriver.ChromeOptions()
 	options.binary_location=r'/Applications/Google Chrome 2.app/Contents/MacOS/Google Chrome'
+
 #undetected_chromedriver require a profile information
 #Sets to create profile in chrome
 #https://stackoverflow.com/questions/50635087/how-to-open-a-chrome-profile-through-user-data-dir-argument-of-selenium
-	options.user_data_dir = r'/Users/amithkanatt/Library/Application Support/Google/Chrome/Profile 1'
+	#options.user_data_dir = r'/Users/amithkanatt/Library/Application Support/Google/Chrome/Profile 1'
 	driver = uc.Chrome(options=options)
 #with helps better exception handling
 #https://www.geeksforgeeks.org/with-statement-in-python/
@@ -26,15 +27,18 @@ if __name__ == '__main__':
 #https://selenium-python.readthedocs.io/locating-elements.html#
 #This option is very resuable one
 		downloadcsv= driver.find_element(By.XPATH,"//img[@title='csv']/parent::a")
+		time.sleep(5)
 		driver.execute_script("arguments[0].click();",downloadcsv)
-
-		driver.quit()
+		time.sleep(5)
+#https://www.browserstack.com/guide/close-browser-in-selenium
+#Closing the browser
+		driver.close()
 #code to move file from one location to another location
 # https://pynative.com/python-move-files/		
-		pattern = 'MW-ETF-'+(datetime.datetime.now()).strftime("%d-%b-%Y")+'.csv'
-		des=r'/Volumes/Project/ETFAnalyser/ETF/ETF_Data/NSE_daily_data'
-		src=r'/Users/amithkanatt/Downloads/'+pattern
-		print(src)
-		shutil.move(src,des)
+	pattern = 'MW-ETF-'+(datetime.datetime.now()).strftime("%d-%b-%Y")+'.csv'
+	des=r'/Volumes/Project/ETFAnalyser/ETF/ETF_Data/NSE_daily_data'
+	src=r'/Users/amithkanatt/Downloads/'+pattern
+	print(src)
+	shutil.move(src,des)
 
 
