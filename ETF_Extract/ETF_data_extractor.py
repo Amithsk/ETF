b0b88878657da92cc314b22aafc81e17925d4f48
Amith <amithsebkanatt@gmail.com>
2022-04-28 00:17:11 +0530

    Updated the chrome path

--- ETF_Extract/ETF_data_extractor.py
+++ ETF_Extract/ETF_data_extractor.py
@@ -1,4 +1,7 @@
 #To extract the ETF's NAV,AUM,Tracking error,Expense ratio
+#This code needs to rewritten to extract the values from  DB rather than file
+#Pending DB table needs to be created to store the URLS of the ETF's
+#How to connect to DB and read the values
 import pandas as PD
 import datetime
 #To bypass the security checks
@@ -12,7 +15,7 @@ from selenium.webdriver.common.by import By
 
 if __name__ == '__main__':
 	options = webdriver.ChromeOptions()
-	options.binary_location=r'/Applications/Google Chrome 2.app/Contents/MacOS/Google Chrome'
+	options.binary_location=r'/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
 #undetected_chromedriver require a profile information
 #Sets to create profile in chrome
 #https://stackoverflow.com/questions/50635087/how-to-open-a-chrome-profile-through-user-data-dir-argument-of-selenium
@@ -27,8 +30,8 @@ if __name__ == '__main__':
 	monthinfo = (datetime.datetime.now()).strftime("%b")
 #Read the url file,using panda
 #Navigate to the location and read the data
-	loc='/Volumes/Project/ETFAnalyser/ETF/ETF_Data/ETF_URL.xlsx'
-	df = PD.read_excel(loc,sheet_name = 'Failed')
+	loc='/Volumes/Project/ETFAnalyser/ETF/ETF_Data/ETF_fund_details/ETF_URLS/ETF_URL.xlsx'
+	df = PD.read_excel(loc,sheet_name = 'Batch1')
 #Loop through the rows using "index",to extract the URL
 #https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.index.html
 	for rwCount in  df.index:
@@ -42,7 +45,7 @@ if __name__ == '__main__':
 #Retrieve the values by looping the list
 #Source:https://stackoverflow.com/questions/28022764/python-and-how-to-get-text-from-selenium-element-webelement-object
 
-			elements = driver.find_elements(By.XPATH,'//div[@class="value typography-body-medium-l   text-15 ellipsis"]')
+			elements = driver.find_elements(By.XPATH,'//div[@class="jsx-3420801268"]')
 			NAV=elements[0].text
 			AUM = elements[1].text
 			Expense_Ratio=elements[2].text
@@ -52,7 +55,7 @@ if __name__ == '__main__':
 #Extract the etfname(it was tricky)
 #Need to add the get_attribute function to get the text information from th H1		
 #https://stackoverflow.com/questions/43429788/python-selenium-finds-h1-element-but-returns-empty-text-string
-			etfName = driver.find_element(By.XPATH,'//div[@class="jsx-1345320326 asset-name-wrapper"]/h1').get_attribute('textContent')
+			etfName = driver.find_element(By.XPATH,'//h1[@class="jsx-1588473315 typography-h4 mb4 ellipsis"]').get_attribute('textContent')
 
 #Append the row details to the data frame using 'append' 
 #https://www.geeksforgeeks.org/how-to-create-an-empty-dataframe-and-append-rows-columns-to-it-in-pandas
@@ -62,7 +65,7 @@ if __name__ == '__main__':
 			print("\n Extacted values AUM",AUM)
 			print("\nETF name",etfName)
 #Write the data into the excel file
-	extracted_value.to_excel(r'/Volumes/Project/ETFAnalyser/ETF/ETF_Data/ETF_fund_details/ETFdetail_'+monthinfo+'.xlsx',index=False)
+	extracted_value.to_excel(r'/Volumes/Project/ETFAnalyser/ETF/ETF_Data/ETF_fund_details/ETFdetails'+monthinfo+'_1'+'.xlsx',index=False)
 #https://www.browserstack.com/guide/close-browser-in-selenium
 #Closing the browser
 	driver.close()
