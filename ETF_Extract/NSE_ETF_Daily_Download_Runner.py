f0430331f6d52e04c6d837a4e10f6f9e898e89c7
Amithsk <amithsebkanat@gmail.com>
2025-05-20 11:49:19 +0530

    updated the code 1/Pickup the right file by updating file name 2/Modified the script to pickup only .csv 3/Git commit update

--- ETF_Extract/NSE_ETF_Daily_Download_Runner.py
+++ ETF_Extract/NSE_ETF_Daily_Download_Runner.py
@@ -9,12 +9,15 @@ os.system(f"python3 {download_script}")
 print("Download script executed.")
 
 # --- Step 2: Move the downloaded file and delete from source ---
-src_folder = "/home/amith/ETF/ETF_Extract"
+src_folder = "/home/amith/"
 dst_folder = "/home/amith/ETF/ETF_Data/Download"
 
-# Find the newest file in the source folder
+# Find the newest file in the source folder specifically check for csv
 files = sorted(
-    [f for f in os.listdir(src_folder) if os.path.isfile(os.path.join(src_folder, f)) and f.startswith("ETF")],
+    [f for f in os.listdir(src_folder)
+     if os.path.isfile(os.path.join(src_folder, f))
+     and f.startswith("ETF_")
+     and f.endswith(".csv")],
     key=lambda x: os.path.getmtime(os.path.join(src_folder, x)),
     reverse=True
 )
@@ -28,12 +31,11 @@ if files:
     shutil.copy2(src_file, dst_file)
     print(f"Copied {latest_file} to Git folder.")
 
-
-
     # --- Step 3: Git commit and push ---
     os.chdir("/home/amith/ETF/ETF_Data")
     subprocess.run(["git", "add", "."], check=True)
     subprocess.run(["git", "commit", "-m", f"Add new ETF file: {latest_file}"], check=False)
+    subprocess.run(["git", "pull", "origin", "main", "--rebase"], check=False)
     subprocess.run(["git", "push", "origin", "main"], check=False)
 
     # Delete original
